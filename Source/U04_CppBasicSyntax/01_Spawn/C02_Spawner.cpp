83f4de27d522a4390632d5456fe43542a1207f9a
포도맛 버섯 <skacjstk0818@gmail.com>
2022-12-30 16:49:21 +0900

    [2022.12.30] 오타수정

--- Source/U04_CppBasicSyntax/01_Spawn/C02_Spawner.cpp
+++ Source/U04_CppBasicSyntax/01_Spawn/C02_Spawner.cpp
@@ -1,28 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "C02_Spawner.h"
 #include "C02_StaticMesh.h"
 
-// Sets default values
 AC02_Spawner::AC02_Spawner()
 {
 
+
 }
 
-// Called when the game starts or when spawned
 void AC02_Spawner::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FTransform transform;
-	for (int32 i = 0; i < 4; ++i) {
-
+	for (int32 i = 0; i < 4; i++)
+	{
+		FTransform transform;
 
 		Meshes[i] = GetWorld()->SpawnActor<AC02_StaticMesh>(SpawnClasses[i], transform);
 
-//		Meshes[i]->GetActorLocation();
-
+		//Meshes[i]->GetActorLocation();
 	}
-	
+
 }
+
--- Source/U04_CppBasicSyntax/01_Spawn/C02_Spawner.h
+++ Source/U04_CppBasicSyntax/01_Spawn/C02_Spawner.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -10,25 +8,22 @@ UCLASS()
 class U04_CPPBASICSYNTAX_API AC02_Spawner : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	AC02_Spawner();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 private:
+	UPROPERTY(EditDefaultsOnly)
+		UClass* ClassRef;
 
 	UPROPERTY(EditDefaultsOnly)
-		UClass* classRef;	// 클래스 레퍼런스를 저장할 수 있는 변수
+		TSubclassOf<class AC02_StaticMesh> TsubclassRef;
 
 	UPROPERTY(EditDefaultsOnly)
-		TSubclassOf<class AC02_Static_Mesh> TsubclassRef;	// 특정 자식으로만 제한할 수 있는 변수
-
-	UPROPERTY(EditDefaultsOnly)
-		TSubclassOf<class AC02_Static_Mesh> SpawnClasses[4];
+		TSubclassOf<class AC02_StaticMesh> SpawnClasses[4];
 
 
 	class AC02_StaticMesh* Meshes[4];
