9c293bb35453ac55eb4efefc362ea9c499909b1c
포도맛 버섯 <skacjstk0818@gmail.com>
2023-01-02 16:53:18 +0900

    [20223.01.02] CLog 복구

--- Source/U04_CppBasicSyntax/Utilities/CLog.h
+++ Source/U04_CppBasicSyntax/Utilities/CLog.h
@@ -1,13 +1,11 @@
-
 #pragma once
 
 #include "CoreMinimal.h"
 
-
 class U04_CPPBASICSYNTAX_API CLog
 {
 public:
-	static void Print(const FString& InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);	// 화면에 찍어주는 용도 
+	static void Print(const FString& InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);	// ��
 	static void Print(int32 InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);
 	static void Print(float InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);
 	static void Print(const FVector& InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);
