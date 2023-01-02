dc5df48fa483b2236bc75c88dcef80b5459e5444
포도맛 버섯 <skacjstk0818@gmail.com>
2023-01-02 17:00:38 +0900

    [2023.01.02] 한글깨짐 방지를 위해 컴퓨터 언어를 UTF-8로 변경

--- Source/U04_CppBasicSyntax/Utilities/CLog.h
+++ Source/U04_CppBasicSyntax/Utilities/CLog.h
@@ -5,7 +5,7 @@
 class U04_CPPBASICSYNTAX_API CLog
 {
 public:
-	static void Print(const FString& InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);	// Å×
+	static void Print(const FString& InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);	// í…ŒìŠ¤íŠ¸
 	static void Print(int32 InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);
 	static void Print(float InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);
 	static void Print(const FVector& InValue, int32 InKey = -1, float InDuration = 5.0f, FColor InColor = FColor::Blue);
